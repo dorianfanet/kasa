e1eaae17ecadbf92d6533f5d50dc82cb4b88b09e
dorianfanet <dorian.fanet@gmail.com>
2022-10-05 17:24:15 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Accommodation from "./pages/Accommodation"
+import Error from "./components/Error"
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/about" element={<About />}></Route>
+      <Route path="/accommodation/:id" element={<Accommodation />}></Route>
+      <Route path="*" element={<Error />}></Route>
+    </Routes>
   );
 }
-
-export default App;
---
+++ src/pages/Accommodation.jsx
@@ -0,0 +1,5 @@
+export default function Accommodation() {
+  return(
+    <div>accomodation</div>
+  )
+}
